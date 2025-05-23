d8653d5a5143241be845e2b2bfa78c203fff7be6
Tao Lu <tao.lu@aquatic.com>
2025-05-23 10:53:33 -0500

    Update guidance.

--- src/types.hpp
+++ src/types.hpp
@@ -4,11 +4,7 @@
 #include <cstdint>
 
 // Feel free to modify this file in any way that makes sense to you,
-// although it'd be nice if the tests in `test.cpp` don't require
-// any changes to run, since we have a few other tests of that form
-// that use these types that we'll paste in and hope to run when
-// evaluating your submission. It's no big deal if we have to manually
-// fix something up, though.
+// but please ensure that the tests in test.cpp continue to run.
 
 namespace aqtc {
 
